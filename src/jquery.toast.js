c96e9c52bc4940dcd1a72475b5cb3be04a2d7393
tom2tom <tpgww@onepost.net>
2018-02-26 19:15:56 +1100

    add API text class names from jq-* to toast-* header (if any) includes closer (if any) call relevant callbacks instead of trigger begin work on element-relative positioning default timeout from 3 to 5 sec

--- src/jquery.toast.js
+++ src/jquery.toast.js
@@ -3,6 +3,51 @@ jQuery toast plugin v.1.4.0
 Copyright (C) 2015-2017 Kamran Ahmed <http://kamranahmed.info>
 License MIT
 */
+/*
+options and their default values:
+ text: '', one, or array of, string(s), text or markup
+ heading: '', string or html
+ showHideTransition: 'fade', type of transition: 'fade', 'slide' or anything else
+ allowToastClose: true, whether to support click-to-close
+ hideAfter: 5000,  dialog lifetime (mS) or false for sticky
+ loader: true, whether to display lifetime progress bar
+ loaderBg: '#9EC600', color of lifetime bar
+ stack: 5, maximum no. of contemporary dialogs
+ position: 'bottom-left', dialog position relative to the refer
+ bgColor: false, CSS property, or false
+ textColor: false, CSS property, or false
+ textAlign: 'left', CSS property, or false
+ icon: false, status-indicator, one of 'success', 'error', 'info', 'warning' (used to suffix applied class "toast-icon-")
+         or else any markup which represents an icon e.g. <img attrs ... />,
+         may be nested e.g. inside <span attrs ...><img attrs ...  /></span> 
+ closeicon: '&times;', string or markup which displays something clickable
+         e.g. '&times;', 'CLOSE', <span attrs ...><img attrs ...  /></span>
+ xclass: false, user-specified class to be applied to the div containing displayed elements
+ drag: false, if sticky (i.e. hideAfter not applied), whether to create a draggable dialog
+ refer: null, reference-element for the dialog, defaults to window
+ beforeShow: null, optional callable, called before dialog is shown, argument is the dialog div object
+ afterShown: null, optional callable, called after dialog is shown, argument is the dialog div object
+ beforeHide: null, optional callable, called before dialog is hidden, argument is the dialog div object
+ afterHidden: null, optional callable, called after dialog is hidden, argument is the dialog div object
+ onClick: null optional callable to process any click anywhere in the dialog (i.e. not just close)
+
+classes:
+'toast-wrap' applied to div containing all created toasts
+'toast-single' applied to each div containing a toast
+settings.position, in 'bottom-left', 'bottom-right', 'top-right', 'top-left', 'bottom-center', 'top-center', 'mid-center'
+settings.xclass applied to the div containing the toast to which this setting applies
+'toast-has-icon' applied to each div containing a toast and which has a type-indicator icon
+'toast-icon-' + 'success', 'error', 'info', 'warning' applied to the type-indicator icon
+'toast-loader' loader-container span identifier
+'toast-loaded' applied to loader-container if loader is true
+'toast-heading' applied to div containing markup and closer (if any)
+'toast-close' applied to settings.closeicon (whatever that is)
+'toast-ul' applied to ul for multi-row message
+'toast-li' applied to each li in multi-row message
+
+ids:
+'toast-item-' + i the li's
+*/
 if ( typeof Object.create !== 'function' ) {
     Object.create = function( obj ) {
         function F() {}
@@ -11,8 +56,8 @@ if ( typeof Object.create !== 'function' ) {
     };
 }
 
-(function( $, window ) { "$:nomunge"
-
+(function( $, window, document ) {
+    "$:nomunge";
     "use strict";
 
     var Toast = {
@@ -45,12 +90,20 @@ if ( typeof Object.create !== 'function' ) {
 
         setup: function () {
 
+            this._toastEl = this._toastEl || $('<div/>', {
+                "class" : "toast-single"
+            });
+            this._toastEl.data('Toastob', this);
+
             var _toastContent = '';
 
-            this._toastEl = this._toastEl || $('<div class="jq-toast-single"></div>');
+            if ( this.options.loader ) {
+                _toastContent += '<span class="toast-loader"></span>';
+            }
 
-            // For the loader on top
-            _toastContent += '<span class="jq-toast-loader"></span>';
+            if ( this.options.heading ) {
+                _toastContent +='<div class="toast-heading">' + this.options.heading;
+            }
 
             if ( this.options.allowToastClose ) {
                 var icon;
@@ -59,25 +112,21 @@ if ( typeof Object.create !== 'function' ) {
                 } else {
                     icon = '&times;';
                 }
-                _toastContent += '<span class="jq-toast-close">' + icon + '</span>';
+                _toastContent += '<span class="toast-close">' + icon + '</span>';
+            }
+
+            if ( this.options.heading ) {
+                _toastContent += '</div>';
             }
 
             if ( this.options.text instanceof Array ) {
-
-                if ( this.options.heading ) {
-                    _toastContent +='<div class="jq-toast-heading">' + this.options.heading + '</div>';
-                }
-
-                _toastContent += '<ul class="jq-toast-ul">';
+                _toastContent += '<ul class="toast-ul">';
                 for (var i = 0; i < this.options.text.length; i++) {
-                    _toastContent += '<li class="jq-toast-li" id="jq-toast-item-' + i + '">' + this.options.text[i] + '</li>';
+                    _toastContent += '<li class="toast-li" id="toast-item-' + i + '">' + this.options.text[i] + '</li>';
                 }
                 _toastContent += '</ul>';
 
             } else {
-                if ( this.options.heading ) {
-                    _toastContent +='<div class="jq-toast-heading">' + this.options.heading + '</div>';
-                }
                 _toastContent += this.options.text;
             }
 
@@ -96,10 +145,10 @@ if ( typeof Object.create !== 'function' ) {
             }
 
             if ( this.options.icon ) {
-                this._toastEl.addClass('jq-has-icon');
+                this._toastEl.addClass('toast-has-icon');
 
                 if ( this._defaultIcons.indexOf(this.options.icon) !== -1 ) {
-                    this._toastEl.addClass('jq-icon-' + this.options.icon);
+                    this._toastEl.addClass('toast-icon-' + this.options.icon);
                 }
             }
 
@@ -109,27 +158,40 @@ if ( typeof Object.create !== 'function' ) {
         },
 
         position: function () {
+
+            var refer, left, top, bottom;
+
+            if ( this.options.refer && this.options.refer instanceof Element ) {
+                refer = this.options.refer;
+            } else {
+                refer = window;
+            } 
             if ( ( typeof this.options.position === 'string' ) && ( this._positionClasses.indexOf(this.options.position) !== -1 ) ) {
 
                 if ( this.options.position === 'bottom-center' ) {
+                    left = Math.max (20, ($(refer).outerWidth() - this._container.outerWidth()) / 2);
+                    top = Math.min (window.outerHeight() - 20, $(refer).bottom() + this._container.outerHeight());
                     this._container.css({
-                        left: ( $(window).outerWidth() / 2 ) - this._container.outerWidth()/2,
-                        bottom: 20
+                        left: left,
+                        top: top
                     });
                 } else if ( this.options.position === 'top-center' ) {
+                    left = Math.max (20, ($(refer).outerWidth() - this._container.outerWidth()) / 2);
+                    top = Math.max (20, ($(refer).top() - this._container.outerHeight()));
                     this._container.css({
-                        left: ( $(window).outerWidth() / 2 ) - this._container.outerWidth()/2,
-                        top: 20
+                        left: left,
+                        top: top
                     });
                 } else if ( this.options.position === 'mid-center' ) {
+                    left = Math.max (20, ($(refer).outerWidth() - this._container.outerWidth()) / 2);
+                    top = Math.max (20, ($(refer).outerHeight() - this._container.outerHeight()) / 2);
                     this._container.css({
-                        left: ( $(window).outerWidth() / 2 ) - this._container.outerWidth()/2,
-                        top: ( $(window).outerHeight() / 2 ) - this._container.outerHeight()/2
+                        left: left,
+                        top: top
                     });
                 } else {
                     this._container.addClass( this.options.position );
                 }
-
             } else if ( typeof this.options.position === 'object' ) {
                 this._container.css({
                     top : this.options.position.top ? this.options.position.top : 'auto',
@@ -138,7 +200,21 @@ if ( typeof Object.create !== 'function' ) {
                     right : this.options.position.right ? this.options.position.right : 'auto'
                 });
             } else {
-                this._container.addClass( 'bottom-left' );
+//                this._container.addClass( 'bottom-left' );
+                this._container.css({
+                   //bottom-left
+                   left: 20,
+                   bottom: 20
+                });
+            }
+        },
+
+        postHide: function () {
+
+            var that = $(this).data('Toastob');
+            
+            if (typeof that.options.afterHidden === 'function') {
+                that.options.afterHidden(this);
             }
         },
 
@@ -146,54 +222,22 @@ if ( typeof Object.create !== 'function' ) {
 
             var that = this;
 
-            this._toastEl.on('afterShown', function () {
-                that.processLoader();
-            });
-
-            this._toastEl.find('.jq-toast-close').on('click', function ( e ) {
+            this._toastEl.find('.toast-close').on('click', function ( e ) {
 
                 e.preventDefault();
 
-                that._toastEl.trigger('beforeHide');
+                if ( typeof that.options.beforeHide === 'function' ) {
+                    that.options.beforeHide(that._toastEl);
+                }
 
                 if( that.options.showHideTransition === 'fade') {
-                    that._toastEl.fadeOut(function () {
-                        that._toastEl.trigger('afterHidden');
-                    });
+                    that._toastEl.fadeOut(that.postHide);
                 } else if ( that.options.showHideTransition === 'slide' ) {
-                    that._toastEl.slideUp(function () {
-                        that._toastEl.trigger('afterHidden');
-                    });
+                    that._toastEl.slideUp(that.postHide);
                 } else {
-                    that._toastEl.hide(function () {
-                        that._toastEl.trigger('afterHidden');
-                    });
+                    that._toastEl.hide(that.postHide);
                 }
             });
-
-            if ( typeof this.options.beforeShow === 'function' ) {
-                this._toastEl.on('beforeShow', function () {
-                    that.options.beforeShow(that._toastEl);
-                });
-            }
-
-            if ( typeof this.options.afterShown === 'function' ) {
-                this._toastEl.on('afterShown', function () {
-                    that.options.afterShown(that._toastEl);
-                });
-            }
-
-            if ( typeof this.options.beforeHide === 'function' ) {
-                this._toastEl.on('beforeHide', function () {
-                    that.options.beforeHide(that._toastEl);
-                });
-            }
-
-            if ( typeof this.options.afterHidden === 'function' ) {
-                this._toastEl.on('afterHidden', function () {
-                    that.options.afterHidden(that._toastEl);
-                });
-            }
 
             if ( typeof this.options.onClick === 'function' ) {
                 this._toastEl.on('click', function () {
@@ -204,11 +248,15 @@ if ( typeof Object.create !== 'function' ) {
 
         addToDom: function () {
 
-             var _container = $('.jq-toast-wrap');
+             var _container = $('.toast-wrap');
 
              if ( _container.length === 0 ) {
 
-                _container = $('<div class="jq-toast-wrap" role="alert" aria-live="polite"></div>');
+                _container = $('<div/>', {
+                    "class" : "toast-wrap",
+                    role : "alert",
+                    "aria-live" : "polite"
+                });
 
                 $('body').append( _container );
 
@@ -216,17 +264,19 @@ if ( typeof Object.create !== 'function' ) {
                 _container.empty();
              }
 
-             _container.find('.jq-toast-single:hidden').remove();
+             _container.find('.toast-single:hidden').remove();
 
              _container.append( this._toastEl );
 
+            this._toastEl.hide();
+
             if ( this.options.stack && !isNaN( parseInt( this.options.stack ), 10 ) ) {
 
-                var _prevToastCount = _container.find('.jq-toast-single').length,
+                var _prevToastCount = _container.find('.toast-single').length,
                     _extToastCount = _prevToastCount - this.options.stack;
 
                 if ( _extToastCount > 0 ) {
-                    $('.jq-toast-wrap').find('.jq-toast-single').slice(0, _extToastCount).remove();
+                    $('.toast-wrap').find('.toast-single').slice(0, _extToastCount).remove();
                 }
 
             }
@@ -244,13 +294,13 @@ if ( typeof Object.create !== 'function' ) {
                 return false;
             }
 
-            var loader = this._toastEl.find('.jq-toast-loader'),
-            // 400 is the default time that jquery uses for fade/slide
-            // Divide by 1000 for milliseconds to seconds conversion
+            var loader = this._toastEl.find('.toast-loader'),
+            // 400 mS is jquery's default duration for fade/slide
+            // Divide by 1000 for mS to S conversion
                 transitionTime = (this.options.hideAfter - 400) / 1000 + 's',
                 loaderBg = this.options.loaderBg,
-
                 style = loader.attr('style') || '';
+
             style = style.substring(0, style.indexOf('-webkit-transition')); // Remove the last transition definition
 
             style += '-webkit-transition:width ' + transitionTime + ' ease-in;' +
@@ -258,52 +308,51 @@ if ( typeof Object.create !== 'function' ) {
                      'transition:width ' + transitionTime + ' ease-in;' +
                      'background-color:' + loaderBg + ';';
 
+            loader.attr('style', style).addClass('toast-loaded');
+        },
 
-            loader.attr('style', style).addClass('jq-toast-loaded');
+        postShow: function () {
+
+            var that = $(this).data('Toastob');
+
+            if (that.options.loader !== false) {
+                that.processLoader();
+            }
+            if (typeof that.options.afterShown === 'function') {
+               that.options.afterShown(this);
+            }
         },
 
         animate: function () {
 
-            var that = this;
-
-            this._toastEl.hide();
-
-            this._toastEl.trigger('beforeShow');
+            if ( typeof this.options.beforeShow === 'function' ) {
+               this.options.beforeShow(this._toastEl);
+            }
 
             if ( this.options.showHideTransition.toLowerCase() === 'fade' ) {
-                this._toastEl.fadeIn(function ( ){
-                    that._toastEl.trigger('afterShown');
-                });
+                this._toastEl.fadeIn(this.postShow);
             } else if ( this.options.showHideTransition.toLowerCase() === 'slide' ) {
-                this._toastEl.slideDown(function ( ){
-                    that._toastEl.trigger('afterShown');
-                });
+                this._toastEl.slideDown(this.postShow);
             } else {
-                this._toastEl.show(function ( ){
-                    that._toastEl.trigger('afterShown');
-                });
+                this._toastEl.show(this.postShow);
             }
 
             if (this.canAutoHide()) {
 
-                that = this;
+               var that = this;
 
                 window.setTimeout(function(){
 
-                    that._toastEl.trigger('beforeHide');
+                    if ( typeof that.options.beforeHide === 'function' ) {
+                       that.options.beforeHide(that._toastEl);
+                    }
 
                     if ( that.options.showHideTransition.toLowerCase() === 'fade' ) {
-                        that._toastEl.fadeOut(function () {
-                            that._toastEl.trigger('afterHidden');
-                        });
+                        that._toastEl.fadeOut(that.postHide);
                     } else if ( that.options.showHideTransition.toLowerCase() === 'slide' ) {
-                        that._toastEl.slideUp(function () {
-                            that._toastEl.trigger('afterHidden');
-                        });
+                        that._toastEl.slideUp(that.postHide);
                     } else {
-                        that._toastEl.hide(function () {
-                            that._toastEl.trigger('afterHidden');
-                        });
+                        that._toastEl.hide(that.postHide);
                     }
 
                 }, this.options.hideAfter);
@@ -313,7 +362,7 @@ if ( typeof Object.create !== 'function' ) {
         reset: function ( resetWhat ) {
 
             if ( resetWhat === 'all' ) {
-                $('.jq-toast-wrap').remove();
+                $('.toast-wrap').remove();
             } else {
                 this._toastEl.remove();
             }
@@ -327,7 +376,7 @@ if ( typeof Object.create !== 'function' ) {
         },
 
         close: function() {
-            this._toastEl.find('.jq-toast-close').click();
+            this._toastEl.find('.toast-close').click();
         }
     };
 
@@ -356,7 +405,7 @@ if ( typeof Object.create !== 'function' ) {
         heading: '',
         showHideTransition: 'fade',
         allowToastClose: true,
-        hideAfter: 3000,
+        hideAfter: 5000,
         loader: true,
         loaderBg: '#9EC600',
         stack: 5,
@@ -365,8 +414,10 @@ if ( typeof Object.create !== 'function' ) {
         textColor: false,
         textAlign: 'left',
         icon: false,
-        closeicon: '&times;', //html or icon-url
+        closeicon: '&times;',
         xclass: false,
+        drag: false,
+        refer: null,
         beforeShow: null,
         afterShown: null,
         beforeHide: null,
@@ -374,4 +425,4 @@ if ( typeof Object.create !== 'function' ) {
         onClick: null
     };
 
-})( jQuery, window );
+})( jQuery, window, document );
